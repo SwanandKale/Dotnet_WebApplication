8e527f09f219959d2ad7d4a29d8d1135b35087bd
SwanandKale <kaleswanand280101@gmail.com>
2023-01-15 19:11:21 +0530

    Delete Component Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import InsertData from "./Component/AddProduct";
-import DisplayProducts from "./Component/DisplayProducts"
+import DisplayProducts from "./Component/DisplayProducts";
+import DeleteProduct from "./Component/DeleteProduct";
 
 function App() {
   return (
     <div>
       <DisplayProducts></DisplayProducts>
       <InsertData></InsertData>
+      <DeleteProduct></DeleteProduct>
+      
     </div>
   );
 }
--- frontend/src/Component/DeleteProduct.js
+++ frontend/src/Component/DeleteProduct.js
@@ -1,6 +1,31 @@
-const DeleteData=()=>
-{
-    return(
+import { useState } from "react";
+import axios from "axios";
+const DeleteData = () => {
+
+    const[apidata,SetApiData]=useState({id:""});
+
+    const handleChange=(event)=>
+    {
+        const{name,value}=event.target;
+        SetApiData({...apidata,[name]:value});
+    }
+
+    const deletedata=(event)=>
+    {
         
+        axios.delete(`http://localhost:5190/api/product/${apidata.id}`);
+    }
+
+
+    return (
+        <div>
+            <br></br>
+            <h3>Enter Id to Delete Product</h3>
+            <form method="GET" onSubmit={deletedata}>
+                <input type="text" name="id" placeholder="Enter Id" onChange={handleChange}></input>
+                <button type="submit">Delete</button>
+            </form>
+        </div>
     );
 }
+export default DeleteData;
